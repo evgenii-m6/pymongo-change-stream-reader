3189b431b07c61794d89e4694a755d58cdc401f2
evgenii-m6 <evgenii.optimal@tutamail.com>
2024-08-03 18:56:47 +0500

    Fix error in WithTimeout

--- pymongo_change_stream_reader/producing/timeout.py
+++ pymongo_change_stream_reader/producing/timeout.py
@@ -11,8 +11,7 @@ class WithTimeout:
 
     def run(self):
         self.thread.start()
-        if self._timeout:
-            self.thread.join(self._timeout)
+        self.thread.join(self._timeout)
         if not self._queue:
             raise TimeoutError()
         return self._queue.popleft()
